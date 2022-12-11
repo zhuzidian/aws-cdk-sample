1f44a3c0f4ca28ccfda24ae2df68f29a793889e3
zhuzidian <zhuzidian@gmail.com>
2022-12-11 10:06:49 +0900

    wip

--- bin/aws-cdk-sample.ts
+++ bin/aws-cdk-sample.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AwsCdkSampleStack } from '../lib/aws-cdk-sample-stack';
+import { HelloStack } from '../lib/aws-cdk-sample-stack';
 
 const app = new cdk.App();
-new AwsCdkSampleStack(app, 'AwsCdkSampleStack', {
+new HelloStack(app, 'HelloStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,8 @@ new AwsCdkSampleStack(app, 'AwsCdkSampleStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // env: { account: '830401256922', region: 'us-east-1' },
+  env: { account: '830401256922', region: 'ap-northeast-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/aws-cdk-sample-stack.ts
+++ lib/aws-cdk-sample-stack.ts
@@ -1,16 +1,91 @@
 import * as cdk from 'aws-cdk-lib';
+import { Tag, Tags } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class AwsCdkSampleStack extends cdk.Stack {
+export class HelloStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    let vpc = new ec2.Vpc(this, "VPC", {
+      vpcName: `${id}-vpc`,
+      maxAzs: 2,
+      natGateways: 0,
+      subnetConfiguration: [
+        // {
+        //   cidrMask: 24,
+        //   name: 'public',
+        //   subnetType: ec2.SubnetType.PUBLIC,
+        // },
+        // {
+        //   cidrMask: 24,
+        //   name: 'private',
+        //   subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        // },
+        // {
+        //   cidrMask: 24,
+        //   name: 'rds',
+        //   subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        // }
+      ]
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkSampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // vpc.publicSubnets.map((subnet, i) => Tags.of(subnet).add("Name", `public-subnet-${i+1}`) )
+    // vpc.privateSubnets.map((subnet, i) => Tags.of(subnet).add("Name", `private-subnet-${i+1}`) )
+
+    let publicRouteTable = new ec2.CfnRouteTable(this, "PublicRouteTable", {
+      vpcId: vpc.vpcId,
+      tags: [{key: "Name", value: "rtb-public"}]
+    })
+    let privateRouteTable = new ec2.CfnRouteTable(this, "PrivateRouteTable", {
+      vpcId: vpc.vpcId,
+      tags: [{key: "Name", value: "rtb-private"}]
+    })
+
+    let publicSubnet1 = new ec2.CfnSubnet(this, "PublicSubnet1", {
+      vpcId: vpc.vpcId,
+      availabilityZone: 'ap-northeast-1a',
+      cidrBlock: "10.0.0.0/24",
+    })
+    Tags.of(publicSubnet1).add("Name", "subnet-public1")
+    
+    let publicSubnet2 = new ec2.CfnSubnet(this, "PublicSubnet2", {
+      vpcId: vpc.vpcId,
+      availabilityZone: 'ap-northeast-1a',
+      cidrBlock: "10.0.1.0/24"
+    })
+    Tags.of(publicSubnet2).add("Name", "subnet-public2")
+
+    new ec2.CfnSubnetRouteTableAssociation(this, 'PublicSubnet1RouteTableAssociation', {
+      routeTableId: publicRouteTable.attrRouteTableId,
+      subnetId: publicSubnet1.attrSubnetId,
+    });
+    new ec2.CfnSubnetRouteTableAssociation(this, 'PublicSubnet2RouteTableAssociation', {
+      routeTableId: publicRouteTable.attrRouteTableId,
+      subnetId: publicSubnet2.attrSubnetId,
+    });
+
+    // sg
+    // new ec2.SecurityGroup(this, "sg-app", {vpc: vpc})
+
+    new ec2.CfnSecurityGroup(this, "sg-db", {
+      groupDescription: 'groupDescription',
+      groupName: 'sg-db',
+      securityGroupIngress: [{
+        ipProtocol: 'tcp',
+        cidrIp: '1.2.3.4/32',
+        fromPort: 22,
+        toPort: 22,
+        description: 'zhu',
+      }],
+      tags: [{
+        key: 'Name',
+        value: 'sg-db',
+      }],
+      vpcId: vpc.vpcId,
+    })
+
   }
 }
